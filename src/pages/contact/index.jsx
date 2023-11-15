e0509174bff415f9c12c3a93cc731da2f3a34bc9
Saky-Dev <garciaisaac468@gmail.com>
2023-11-15 10:53:20 -0600

    feat: added functionality to buttons on contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
 import './index.sass'
 
 const Contact = () => {
+  const [message, setMessage] = useState('')
+
+  const handleClearMessage = () => {
+    setMessage('')
+    document.getElementById('user-message').focus()
+  }
+
+  const handleUpdateMessage = e => {
+    setMessage(e.currentTarget.value)
+  }
+
+  const handleSendMessage = () => {
+
+  }
+
   return (
     <div className='contact'>
       <h1>Contact</h1>
       <p>Write us a message with your opinion, suggestion or disagreement, no names or unnecessaries things</p>
       <div className='message'>
-        <textarea name='user-message' id='user-message' placeholder='Message' />
+        <textarea name='user-message' id='user-message' placeholder='Message' onInput={handleUpdateMessage} value={message} />
         <div className='actions'>
-          <button className='ghost'>Clear</button>
-          <button>Send</button>
+          <button className='ghost' onClick={handleClearMessage}>Clear</button>
+          <button onClick={handleSendMessage}>Send</button>
         </div>
       </div>
     </div>
