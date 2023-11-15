4846d6c891d8924d5bb6319e0ada0bb04974447d
Saky-Dev <garciaisaac468@gmail.com>
2023-11-15 09:23:57 -0600

    feat: created contact files
    changed height measure of about and created contact
    files structure

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,19 @@
+import './index.sass'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <h1>Contact</h1>
+      <p>Write us a message with your opinion, suggestion or disagreement, no names or unnecessaries things</p>
+      <div className='message'>
+        <textarea name='user-message' id='user-message' placeholder='Message' />
+        <div className='actions'>
+          <button>Clear</button>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
